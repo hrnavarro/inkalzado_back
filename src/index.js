e3aa1d7c5234d8a8379360faa7fd6cb14b70938b
jimmy <jimmy20232024m@gamil.comm>
2023-11-22 00:49:45 -0500

    otro cambio

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 //llamar paquetes
 const express= require("express")
 const mongoose=require("mongoose")
+
 require("dotenv").config()
+const zapatillas= require("./routes/carrito_compra")
 
 //Definicion de configuraciones
 const app=express()
 const puerto = 7000;
+app.use(express.json());
+//middleware
+
+
 //rutas
+app.get("/",(req,res)=>{res.send("probando puerto")})
+app.use("/api",zapatillas)
 
 
 //Ejecucion
-mongoose.connect(process.env.mongodb)
+mongoose.connect("mongodb+srv://hrodrigueznavarro62:nqbRN3kbM3OXO6C2@cluster0.bqagj5g.mongodb.net/inkalzado?retryWrites=true&w=majority")
+.then(() => {console.log("conexion con exito siuu")})
+.catch((error)=>{console.log("no hay noexion")})
 
-app.get("/inkalzado/express",(req,res)=>(res.send("Hello world")))
-
-app.listen(puerto,()=>{console.log("servidor escuchando en el puerto"+puerto)});
+app.listen(puerto,()=>{console.log("puerto en "+puerto)})
--- src/routes/carrito_compra.js
+++ src/routes/carrito_compra.js
@@ -7,7 +7,28 @@ router.get("/zapatillas",(req,res)=>(zapatillasModel.find()
     .then((data)=>res.json(data))
     
     ))
+
+
+
+
+   //getby,id
+   router.get("/zapatillas/:id",(req,res)=>{
+    const zapatilas= zapatillasModel(req,body);
+    zapatilas.save()
+    .then((data)=>res.json({mensaje:"compra realizada"}))
+    .catch((error)=>res.json({mensaje:"error"}))
+})   
+   
+
 //post
+router.post("/zapatillas",(req,res)=>{
+    const zapatilas= zapatillasModel(req,body);
+    zapatilas.save()
+    .then((data)=>res.json({mensaje:"compra realizada"}))
+    .catch((error)=>res.json({mensaje:"error"}))
+})
+
+
 
 //put
 
